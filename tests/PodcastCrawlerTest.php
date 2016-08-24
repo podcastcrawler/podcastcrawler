2f0687c1a31f82251d859432c4626a94a9c2a3e7
Dorian Neto <doriansampaioneto@gmail.com>
2016-08-24 13:01:28 -0300

    test(main): added tests to feed

--- tests/PodcastCrawlerTest.php
+++ tests/PodcastCrawlerTest.php
@@ -44,6 +44,7 @@ class PodcastCrawlerTest extends PHPUnit
         $this->assertInternalType('array', $list_decoded);
         $this->assertArrayHasKey('resultCount', $list_decoded);
         $this->assertArrayHasKey('results', $list_decoded);
+        $this->assertEquals(200, $this->instance->getStatusCode());
     }
 
     /**
@@ -79,7 +80,6 @@ class PodcastCrawlerTest extends PHPUnit
         $list_decoded = json_decode($list, true);
 
         $this->assertInternalType('string', $list);
-        $this->assertInternalType('int', self::ID);
         $this->assertInternalType('array', $list_decoded);
         $this->assertArrayHasKey('resultCount', $list_decoded);
         $this->assertArrayHasKey('results', $list_decoded);
@@ -105,6 +105,51 @@ class PodcastCrawlerTest extends PHPUnit
             $this->assertArrayHasKey('artworkUrl600', $item);
             $this->assertArrayHasKey('country', $item);
             $this->assertArrayHasKey('primaryGenreName', $item);
+        }
+    }
+
+    /**
+     * Test to validate the return of the feed sought by the id
+     * @runInSeparateProcess
+     */
+    public function testFeed()
+    {
+        $list = $this->instance->feed(self::ID);
+        $list_decoded = json_decode($list, true);
+
+        $this->assertInternalType('string', $list);
+        $this->assertInternalType('array', $list_decoded);
+        $this->assertEquals(200, $this->instance->getStatusCode());
+        $this->assertArrayHasKey('itunes_id', $list_decoded);
+        $this->assertArrayHasKey('title', $list_decoded);
+        $this->assertArrayHasKey('description', $list_decoded);
+        $this->assertArrayHasKey('image', $list_decoded);
+        $this->assertInternalType('array', $list_decoded['links']);
+        $this->assertArrayHasKey('links', $list_decoded);
+        $this->assertArrayHasKey('site', $list_decoded['links']);
+        $this->assertArrayHasKey('rss', $list_decoded['links']);
+        $this->assertArrayHasKey('itunes', $list_decoded['links']);
+        $this->assertArrayHasKey('genre', $list_decoded);
+        $this->assertArrayHasKey('language', $list_decoded);
+        $this->assertArrayHasKey('episodes', $list_decoded);
+    }
+
+    /**
+     * Test to validate the return of the fields in the feed sought by the id
+     * @runInSeparateProcess
+     */
+    public function testFeedMp3Fields()
+    {
+        $list = json_decode($this->instance->feed(self::ID), true);
+
+        $this->assertGreaterThan(0, $list['episodes']);
+
+        foreach ($list['mp3'] as $item) {
+            $this->assertArrayHasKey('title', $item);
+            $this->assertArrayHasKey('mp3', $item);
+            $this->assertArrayHasKey('description', $item);
+            $this->assertArrayHasKey('link', $item);
+            $this->assertArrayHasKey('published_at', $item);
         }
     }
 }
